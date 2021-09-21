10121e26c32168d065ce8b0f093f9ee52e12cfb7
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-21 21:14:13 +0200

    Create HelloWorld.vue

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <img class="header-img" alt="Vue logo" src="../assets/header-1.png" />
+    <div>
+      <h1>
+        My name is <span class="orange-text"> Lucas </span> & I’m a <br />
+        <span class="orange-text"> multimedia designer </span> student
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header-img {
+  margin-top: 10rem;
+  width: 15%;
+  height: auto;
+}
+
+.orange-text {
+  color: #d7793e;
+}
+
+h1 {
+  color: #fff;
+  padding-bottom: 200px;
+  font-size: 40px;
+}
+</style>
