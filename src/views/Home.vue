d04a0ecfdff4d527747f02f7e0ed30d5b46eb606
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-21 21:14:04 +0200

    Create Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div :class="{ changecolor: scrollPosition > 100 }" class="home">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <AboutMeHome />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import AboutMeHome from "@/components/AboutMeHome.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+    AboutMeHome,
+  },
+  data() {
+    return {
+      scrollPosition: null,
+    };
+  },
+  methods: {
+    updateScroll() {
+      this.scrollPosition = window.scrollY;
+    },
+  },
+  mounted() {
+    window.addEventListener("scroll", this.updateScroll);
+  },
+};
+</script>
+<style scoped>
+.home {
+  background-color: #000;
+  color: white;
+}
+
+.changecolor {
+  background-color: white;
+  color: #000;
+  transition: 1.5s;
+}
+</style>
