4a86adbfb95d1e450b2fc285b0e837a7bdef323a
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-21 21:44:19 +0200

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,6 +7,7 @@
         <span class="orange-text"> multimedia designer </span> student
       </h1>
     </div>
+    <img class="scroll-img" alt="Vue logo" src="../assets/scroll-icon@2x.png" />
   </div>
 </template>
 
@@ -27,7 +28,13 @@
 
 h1 {
   color: #fff;
-  padding-bottom: 200px;
   font-size: 40px;
+  padding-bottom: 100px;
+}
+
+.scroll-img {
+  width: 2%;
+  height: auto;
+  padding-bottom: 200px;
 }
 </style>
