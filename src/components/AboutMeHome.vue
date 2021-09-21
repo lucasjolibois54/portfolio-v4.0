f5952b24894ad472820d85a64827e32a909691aa
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-21 21:14:16 +0200

    Create AboutMeHome.vue

---
+++ src/components/AboutMeHome.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="center bbc-w text-padding">
+    <h2 class="h2-padding">About me</h2>
+    <p>
+      Hi! My name is Lucas and I enjoy designing & creating things that live on
+      the internet. My interest in informatics started back in 2017 when I
+      decided To study computer science in highschool. Back then I was building
+      & Developing circuits with arduino. — All of that led me to where I am
+      now! At the moment I am a student at Zealand Business Academy, studying
+      Multimedia designer. I have developed at larger reach of interest within
+      Web- development and graphic design. At the moment my biggest Interests
+      are in UX & UI- design, web- development, logo design, Graphic design and
+      things within that niche.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.center {
+  text-align: center;
+}
+
+.bbc-w {
+  background-color: white;
+}
+
+.text-padding {
+  padding-right: 20%;
+  padding-left: 20%;
+}
+
+.h2-padding {
+  padding-top: 50px;
+}
+</style>
