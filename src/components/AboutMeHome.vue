463c9d03c9983da57fb3aec5206a2acbf41a2baa
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-21 21:44:16 +0200

    Update AboutMeHome.vue

--- src/components/AboutMeHome.vue
+++ src/components/AboutMeHome.vue
@@ -12,6 +12,7 @@
       are in UX & UI- design, web- development, logo design, Graphic design and
       things within that niche.
     </p>
+    <button id="pad-btn" class="btn-grad">Learn more about me</button>
   </div>
 </template>
 
@@ -39,5 +40,46 @@ h2 {
 
 p {
   font-size: 20px;
+}
+
+.btn-grad {
+  background-image: linear-gradient(
+    to right,
+    #ff512f 0%,
+    #f09819 51%,
+    #ff512f 100%
+  );
+  border: none;
+  cursor: pointer;
+}
+.btn-grad {
+  text-align: center;
+  text-transform: uppercase;
+  transition: 0.5s;
+  background-size: 200% auto;
+  color: white;
+  box-shadow: 0px 4px 5px 0px rgba(0, 0, 0, 0.1),
+    0px 5px 20px 0px rgba(0, 0, 0, 0.1);
+  display: block;
+  text-decoration: none;
+  display: inline-block;
+  padding: 0.7375em 0.99em;
+  border-radius: 2em;
+  font-size: 14px;
+  margin: 0.5em 0;
+  padding-bottom: 0.7375em;
+  box-shadow: 0px 4px 5px 0px rgba(0, 0, 0, 0.1),
+    0px 1px 20px 0px rgba(0, 0, 0, 0.1);
+}
+
+.btn-grad:hover {
+  background-position: right center; /* changes the direction of the grad on button */
+  color: #fff;
+  text-decoration: none;
+  box-shadow: 0px 0px #888888;
+}
+
+#pad-btn {
+  margin-bottom: 200px;
 }
 </style>
