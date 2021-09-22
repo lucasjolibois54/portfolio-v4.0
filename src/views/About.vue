fc87c16bcc836bc9449e7b8cedfb959c4ba78012
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-22 23:31:39 +0200

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,58 @@
+
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <NavBar />
+    <UnvoitHero
+      :class="{ changecolor: scrollPosition > 70 }"
+      class="home"
+      msg="Welcome to Your Vue.js App"
+    />
+    <UnvoitMeaning />
+
+    <Footer />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import UnvoitHero from "@/components/UnvoitProject/UnvoitHero.vue";
+import NavBar from "@/components/NavBar.vue";
+import Footer from "@/components/Footer.vue";
+import UnvoitMeaning from "@/components/UnvoitProject/UnvoitMeaning.vue";
+
+export default {
+  name: "Home",
+  components: {
+    UnvoitHero,
+    NavBar,
+    Footer,
+    UnvoitMeaning,
+  },
+  data() {
+    return {
+      scrollPosition: null,
+    };
+  },
+  methods: {
+    updateScroll() {
+      this.scrollPosition = window.scrollY;
+    },
+  },
+  mounted() {
+    window.addEventListener("scroll", this.updateScroll);
+  },
+};
+</script>
+<style scoped>
+.home {
+  background-color: #000;
+  color: white;
+}
+
+.changecolor {
+  background-color: white;
+  color: black;
+  transition: 1.5s;
+}
+</style>
