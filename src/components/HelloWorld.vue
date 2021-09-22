0984994062762757d86b8e7cf31cf68bebd7d7d2
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-22 20:28:01 +0200

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,21 @@
 <template>
-  <div>
+  <div class="flex flex-col justify-center items-center">
     <img
-      class="header-img content-center"
+      class="header-img h-auto pb-10 pt-20"
       alt="Vue logo"
       src="../assets/header-1.png"
     />
     <div>
-      <h1>
+      <h1 class="text-3xl">
         My name is <span class="orange-text"> Lucas </span> & I’m a <br />
         <span class="orange-text"> multimedia designer </span> student
       </h1>
     </div>
-    <img class="scroll-img" alt="Vue logo" src="../assets/scroll-icon@2x.png" />
+    <img
+      class="scroll-img h-auto pb-10 pt-10"
+      alt="Vue logo"
+      src="../assets/scroll-icon@2x.png"
+    />
   </div>
 </template>
 
@@ -20,4 +24,11 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.header-img {
+  width: 17%;
+}
+
+.scroll-img {
+  width: 2%;
+}
 </style>
