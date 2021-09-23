be79c57e365d73f72387e13e197f91b828485923
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-24 00:36:43 +0200

    Update UnvoitDo.vue

--- src/components/UnvoitProject/UnvoitDo.vue
+++ src/components/UnvoitProject/UnvoitDo.vue
@@ -1,13 +1,20 @@
 <template>
-  <div class="flex flex-col justify-center items-center">
-    <img
-      class="scroll-img w-5/12 h-auto pb-10 pt-32"
-      :src="require('@/assets/unvoit-do.png')"
-    />
+  <div class="width-hero">
+    <div class="flex flex-col justify-center items-center">
+      <img
+        class="scroll-img w-5/12 h-auto pb-10 pt-32"
+        :src="require('@/assets/unvoit-do.png')"
+      />
+    </div>
   </div>
 </template>
 
 <style scoped>
+.width-hero {
+  max-width: 1400px;
+  margin: 0 auto !important;
+  float: none !important;
+}
 @media only screen and (max-width: 650px) and (min-width: 10px) {
   img {
     width: 80%;
