0597293f67649494d98477de68cf6755288c4bde
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-24 00:36:37 +0200

    Update AboutMeHome.vue

--- src/components/AboutMeHome.vue
+++ src/components/AboutMeHome.vue
@@ -1,26 +1,34 @@
 <template>
-  <div class="center bbc-w text-padding">
-    <h2 class="h2-padding">About me</h2>
-    <p>
-      Hi! My name is Lucas and I enjoy designing & creating things that live on
-      the internet. My interest in informatics started back in 2017 when I
-      decided To study computer science in highschool. Back then I was building
-      & Developing circuits with arduino. <br /><br />
-      — All of that led me to where I am now! At the moment I am a student at
-      Zealand Business Academy, studying Multimedia designer. I have developed
-      at larger reach of interest within Web- development and graphic design. At
-      the moment my biggest Interests are in UX & UI- design, web- development,
-      logo design, Graphic design and things within that niche.
-    </p>
-    <router-link to="/About"
-      ><button id="pad-btn" class="btn-grad">
-        Learn more about me
-      </button></router-link
-    >
+  <div class="width-hero">
+    <div class="center bbc-w text-padding">
+      <h2 class="h2-padding">About me</h2>
+      <p>
+        Hi! My name is Lucas and I enjoy designing & creating things that live
+        on the internet. My interest in informatics started back in 2017 when I
+        decided To study computer science in highschool. Back then I was
+        building & Developing circuits with arduino. <br /><br />
+        — All of that led me to where I am now! At the moment I am a student at
+        Zealand Business Academy, studying Multimedia designer. I have developed
+        at larger reach of interest within Web- development and graphic design.
+        At the moment my biggest Interests are in UX & UI- design, web-
+        development, logo design, Graphic design and things within that niche.
+      </p>
+      <router-link to="/About"
+        ><button id="pad-btn" class="btn-grad">
+          Learn more about me
+        </button></router-link
+      >
+    </div>
   </div>
 </template>
 
 <style scoped>
+.width-hero {
+  max-width: 1400px;
+  margin: 0 auto !important;
+  float: none !important;
+}
+
 .center {
   text-align: center;
 }
