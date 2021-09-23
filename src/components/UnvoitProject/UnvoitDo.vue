71e85e2221c3b3990b7f24bb9c15982985bfa879
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-23 22:29:29 +0200

    Create UnvoitDo.vue

---
+++ src/components/UnvoitProject/UnvoitDo.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex flex-col justify-center items-center">
+    <img
+      class="scroll-img w-5/12 h-auto pb-10 pt-32"
+      :src="require('@/assets/unvoit-do.png')"
+    />
+  </div>
+</template>
