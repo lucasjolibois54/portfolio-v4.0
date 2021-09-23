f7b2b618a3202175aa61d689ed5b976764ecb349
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-24 00:36:45 +0200

    Update UnvoitConclusion.vue

--- src/components/UnvoitProject/UnvoitConclusion.vue
+++ src/components/UnvoitProject/UnvoitConclusion.vue
@@ -1,44 +1,54 @@
 <template>
-  <div class="bg-gray-800 text-white">
-    <h2
-      class="
-        text-4xl
-        flex flex-col
-        justify-center
-        items-center
-        pr-36
-        pl-36
-        pt-16
-        pb-1
-      "
-    >
-      Conclusion
-    </h2>
+  <div class="bg-gray-800">
+    <div class="width-hero">
+      <div class="bg-gray-800 text-white">
+        <h2
+          class="
+            text-4xl
+            flex flex-col
+            justify-center
+            items-center
+            pr-36
+            pl-36
+            pt-16
+            pb-1
+          "
+        >
+          Conclusion
+        </h2>
 
-    <div>
-      <h3
-        class="
-          text-2xl
-          flex flex-col
-          justify-center
-          items-center
-          pr-36
-          pl-36
-          pt-5
-          pb-8
-        "
-      >
-        This is the first project that I have been working on using vue.js and
-        it has gone pretty well. The whole project was based on Lean startup as
-        well as designthing inwhich we used throughout the whole process. I’m
-        pretty happy about the results of this project and there isn’t really
-        much that could be improved.
-      </h3>
+        <div>
+          <h3
+            class="
+              text-2xl
+              flex flex-col
+              justify-center
+              items-center
+              pr-36
+              pl-36
+              pt-5
+              pb-8
+            "
+          >
+            This is the first project that I have been working on using vue.js
+            and it has gone pretty well. The whole project was based on Lean
+            startup as well as designthing inwhich we used throughout the whole
+            process. I’m pretty happy about the results of this project and
+            there isn’t really much that could be improved.
+          </h3>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.width-hero {
+  max-width: 1400px;
+  margin: 0 auto !important;
+  float: none !important;
+}
+
 @media only screen and (max-width: 650px) and (min-width: 10px) {
   h3 {
     font-size: 17px;
