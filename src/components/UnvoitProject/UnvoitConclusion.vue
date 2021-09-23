0927d8913497d2b4cd16456dbd2bf3ffce45129c
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-23 22:29:30 +0200

    Create UnvoitConclusion.vue

---
+++ src/components/UnvoitProject/UnvoitConclusion.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="bg-gray-800 text-white">
+    <h2
+      class="
+        text-4xl
+        flex flex-col
+        justify-center
+        items-center
+        pr-36
+        pl-36
+        pt-16
+        pb-1
+      "
+    >
+      Conclusion
+    </h2>
+
+    <div>
+      <h2
+        class="
+          text-2xl
+          flex flex-col
+          justify-center
+          items-center
+          pr-36
+          pl-36
+          pt-5
+          pb-8
+        "
+      >
+        This is the first project that I have been working on using vue.js and
+        it has gone pretty well. The whole project was based on Lean startup as
+        well as designthing inwhich we used throughout the whole process. I’m
+        pretty happy about the results of this project and there isn’t really
+        much that could be improved.
+      </h2>
+    </div>
+  </div>
+</template>
