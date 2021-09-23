07beec34ef2ff853823c700f4b861e21a068a2d0
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-24 00:09:19 +0200

    Update UnvoitMeaning.vue

--- src/components/UnvoitProject/UnvoitMeaning.vue
+++ src/components/UnvoitProject/UnvoitMeaning.vue
@@ -1,28 +1,36 @@
 <template>
-  <div class="flex flex-wrap overflow-hidden mt-36 mb-36 ml-10 mr-10">
-    <div class="w-full overflow-hidden md:w-1/2 lg:w-1/2 xl:w-1/2">
-      <!-- Column Content -->
-      <div class="flex flex-col justify-center items-center">
-        <img
-          class="scroll-img w-11/12 h-auto pl-20"
-          :src="require('@/assets/UNVOIT-phone-side.gif')"
-        />
+  <div class="width-hero">
+    <div class="flex flex-wrap overflow-hidden mt-36 mb-36 ml-10 mr-10">
+      <div class="w-full overflow-hidden md:w-1/2 lg:w-1/2 xl:w-1/2">
+        <!-- Column Content -->
+        <div class="flex flex-col justify-center items-center">
+          <img
+            class="scroll-img w-11/12 h-auto pl-20"
+            :src="require('@/assets/UNVOIT-phone-side.gif')"
+          />
+        </div>
       </div>
-    </div>
 
-    <div class="w-full overflow-hidden md:w-1/2 lg:w-1/2 xl:w-1/2">
-      <!-- Column Content -->
-      <h2 class="text-3xl pr-40 pt-10">
-        The meaning of this project, was to develop an invoice management SPA
-        SaaS web application, to help businesses manage their invoices for their
-        clients as well as to send invoices and payment requests.
-      </h2>
+      <div class="w-full overflow-hidden md:w-1/2 lg:w-1/2 xl:w-1/2">
+        <!-- Column Content -->
+        <h2 class="text-3xl pr-40 pt-10">
+          The meaning of this project, was to develop an invoice management SPA
+          SaaS web application, to help businesses manage their invoices for
+          their clients as well as to send invoices and payment requests.
+        </h2>
+      </div>
     </div>
   </div>
 </template>
 
 
 <style scoped>
+.width-hero {
+  max-width: 1300px;
+  margin: 0 auto !important;
+  float: none !important;
+}
+
 @media only screen and (max-width: 650px) and (min-width: 10px) {
   h2 {
     padding-right: 0;
@@ -32,6 +40,41 @@
   img {
     padding-right: 0;
     padding-left: 0;
+  }
+}
+
+@media only screen and (max-width: 1000px) and (min-width: 651px) {
+  h2 {
+    font-size: 20px;
+    padding-right: 5px;
+    line-height: 25px;
+  }
+  img {
+    width: 80%;
+    margin-top: 30px;
+    padding-left: 5px;
+  }
+}
+
+@media only screen and (max-width: 1200px) and (min-width: 1000px) {
+  h2 {
+    font-size: 30px;
+    padding-right: 5px;
+    line-height: 33px;
+  }
+  img {
+    width: 80%;
+    margin-top: 40px;
+    padding-left: 5px;
+  }
+}
+
+@media only screen and (max-width: 1300px) and (min-width: 1200px) {
+  h2 {
+    font-size: 30px;
+    padding-right: 0px;
+    line-height: 33px;
+    padding-top: 40px;
   }
 }
 </style>
