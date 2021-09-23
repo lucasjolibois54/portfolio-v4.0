b84ca433a506c1a7cefe3917df1e0a70924878c5
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-24 00:36:49 +0200

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,25 @@
 <template>
-  <div class="flex flex-col justify-center items-center">
-    <img
-      class="header-img h-auto pb-10 pt-20"
-      alt="Vue logo"
-      src="../assets/header-1.png"
-    />
-    <div>
-      <h1 class="text-3xl">
-        My name is <span class="orange-text"> Lucas </span> & I’m a <br />
-        <span class="orange-text"> multimedia designer </span> student
-      </h1>
+  <div class="bg-black">
+    <div class="width-hero">
+      <div class="flex flex-col justify-center items-center">
+        <img
+          class="header-img h-auto pb-10 pt-20"
+          alt="Vue logo"
+          src="../assets/header-1.png"
+        />
+        <div>
+          <h1 class="text-3xl">
+            My name is <span class="orange-text"> Lucas </span> & I’m a <br />
+            <span class="orange-text"> multimedia designer </span> student
+          </h1>
+        </div>
+        <img
+          class="scroll-img h-auto pb-10 pt-10"
+          alt="Vue logo"
+          src="../assets/scroll-icon@2x.png"
+        />
+      </div>
     </div>
-    <img
-      class="scroll-img h-auto pb-10 pt-10"
-      alt="Vue logo"
-      src="../assets/scroll-icon@2x.png"
-    />
   </div>
 </template>
 
@@ -24,6 +28,12 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.width-hero {
+  max-width: 1400px;
+  margin: 0 auto !important;
+  float: none !important;
+}
+
 .header-img {
   width: 17%;
 }
