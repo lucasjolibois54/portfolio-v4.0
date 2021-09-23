e310fafa19530398d3ac7db57eff2c05f32f3629
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-23 22:29:27 +0200

    Create UnvoitIDid.vue

---
+++ src/components/UnvoitProject/UnvoitIDid.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex flex-wrap overflow-hidden mt-36 mb-36 ml-10 mr-10">
+    <div class="w-full overflow-hidden md:w-1/2 lg:w-1/2 xl:w-1/2">
+      <!-- Column Content -->
+      <h2 class="text-3xl pl-40 pt-10">
+        I developed the web application Using Vue.js, and on top of that we
+        Added a CSSS library called tailwind And used firebase for the sign up
+        Fuctionality on the SAAS we app.
+      </h2>
+    </div>
+
+    <div class="w-full overflow-hidden md:w-1/2 lg:w-1/2 xl:w-1/2">
+      <!-- Column Content -->
+
+      <div class="flex flex-col justify-center items-center">
+        <img
+          class="scroll-img w-11/12 h-auto pr-20"
+          :src="require('@/assets/UNVOIT-phone-side.gif')"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+@media only screen and (max-width: 650px) and (min-width: 10px) {
+}
+</style>
