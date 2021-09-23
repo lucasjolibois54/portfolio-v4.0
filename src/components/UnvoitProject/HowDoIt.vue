d9247d4c4756cde0d92ea174093200fb7d65ad1a
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-23 19:16:10 +0200

    Create HowDoIt.vue

---
+++ src/components/UnvoitProject/HowDoIt.vue
@@ -0,0 +1,22 @@
+<template>
+  <h3 class="text-6xl pb-20 mt-40">
+    HOW DID I DO IT? <br />
+    HOW DID I DO IT? <br />
+    HOW DID I DO IT? <br />
+    HOW DID I DO IT?
+  </h3>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Kanit:wght@900&family=Roboto:wght@900&family=Work+Sans:wght@900&display=swap");
+</style>
+
+<style scoped>
+h3 {
+  font-family: "Kanit", sans-serif;
+  color: black;
+  -webkit-text-fill-color: white; /* Will override color (regardless of order) */
+  -webkit-text-stroke-width: 1.5px;
+  -webkit-text-stroke-color: #d7793e;
+}
+</style>
