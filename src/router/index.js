164a266fca53b2827a8bce21d7f1254004a15d68
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-24 16:37:24 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -25,7 +25,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Projects.vue')
-  }
+  }, {
+    path: '/Ybrnt',
+    name: 'Ybrnt',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Ybrnt.vue')
+  },
 ]
 
 const router = new VueRouter({
