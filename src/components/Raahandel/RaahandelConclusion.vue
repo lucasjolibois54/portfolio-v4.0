de7016619c5b3c59afbb03f630e0b18808386f31
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-25 00:12:20 +0200

    Create RaahandelConclusion.vue

---
+++ src/components/Raahandel/RaahandelConclusion.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="bg-gray-800">
+    <div class="width-hero">
+      <div class="bg-gray-800 text-white">
+        <h2
+          class="
+            text-4xl
+            flex flex-col
+            justify-center
+            items-center
+            pr-36
+            pl-36
+            pt-16
+            pb-1
+          "
+        >
+          Conclusion
+        </h2>
+
+        <div>
+          <h3
+            class="
+              text-2xl
+              flex flex-col
+              justify-center
+              items-center
+              pr-36
+              pl-36
+              pt-5
+              pb-8
+            "
+          >
+            This was an interesting project, as i presented the results of me
+            designs to the owner of Raahandler, in which he really seemed to
+            like. Unfortunately they weren't ready for a total redesign as they
+            didn't quite have that resources for that just yet.
+          </h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.width-hero {
+  max-width: 1400px;
+  margin: 0 auto !important;
+  float: none !important;
+}
+
+@media only screen and (max-width: 650px) and (min-width: 10px) {
+  h3 {
+    font-size: 17px;
+    padding-right: 15px;
+    padding-left: 15px;
+  }
+  h2 {
+    font-size: 27px;
+  }
+}
+</style>
