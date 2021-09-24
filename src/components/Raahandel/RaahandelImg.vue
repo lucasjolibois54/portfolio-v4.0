aceaf0f258ccff776831c1f359707e99063fe76d
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-25 00:12:14 +0200

    Create RaahandelImg.vue

---
+++ src/components/Raahandel/RaahandelImg.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="width-hero">
+    <div class="flex flex-col justify-center items-center">
+      <img
+        class="scroll-img w-5/12 h-auto mb-10 pt-32"
+        :src="require('@/assets/mockuo-1-raahandel.png')"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.width-hero {
+  max-width: 1400px;
+  margin: 0 auto !important;
+  float: none !important;
+}
+@media only screen and (max-width: 650px) and (min-width: 10px) {
+  img {
+    width: 80%;
+    padding-top: 70px;
+  }
+}
+</style>
