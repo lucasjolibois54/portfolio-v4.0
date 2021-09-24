f8f6582647082be6b0f39fd69c4f3eddb9100dd7
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-24 16:37:22 +0200

    Create Ybrnt.vue

---
+++ src/views/Ybrnt.vue
@@ -0,0 +1,51 @@
+
+
+<template>
+  <div>
+    <NavBar />
+    <YbrntHero :class="{ changecolor: scrollPosition > 70 }" class="home" />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import YbrntHero from "@/components/YBRNT/YbrntHero.vue";
+import NavBar from "@/components/NavBar.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "Home",
+  components: {
+    YbrntHero,
+    NavBar,
+    Footer,
+  },
+  data() {
+    return {
+      scrollPosition: null,
+    };
+  },
+  methods: {
+    updateScroll() {
+      this.scrollPosition = window.scrollY;
+    },
+  },
+  mounted() {
+    window.addEventListener("scroll", this.updateScroll);
+  },
+};
+</script>
+<style scoped>
+.home {
+  background-color: #000;
+  color: white;
+}
+
+.changecolor {
+  background-color: white;
+  color: black;
+  border: black;
+  transition: 1.5s;
+}
+</style>
