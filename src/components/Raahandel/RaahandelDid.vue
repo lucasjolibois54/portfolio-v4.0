030753846104fdd5eaf4cf709b5ef3443c17aa04
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-25 00:12:18 +0200

    Create RaahandelDid.vue

---
+++ src/components/Raahandel/RaahandelDid.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="width-hero">
+    <div class="flex flex-wrap overflow-hidden mt-36 mb-36 ml-10 mr-10 nam-div">
+      <div class="w-full overflow-hidden md:w-1/2 lg:w-1/2 xl:w-1/2">
+        <!-- Column Content -->
+        <h2 class="text-2xl pl-40 pt-10">
+          I started off with the usual research by folllowing the process method
+          design thinking. Afterwards I started redesigning their website, and
+          tried to understand their customer journey as well as which elements
+          to implement on the website compared to what they have to offer as
+          their previous website wasn't resonating with anyone.
+        </h2>
+      </div>
+
+      <div class="w-full overflow-hidden md:w-1/2 lg:w-1/2 xl:w-1/2">
+        <!-- Column Content -->
+
+        <div class="flex flex-col justify-center items-center">
+          <img
+            class="scroll-img w-11/12 h-auto pr-20"
+            :src="require('@/assets/sec-unvoit-movie.gif')"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+.width-hero {
+  max-width: 1400px;
+  margin: 0 auto !important;
+  float: none !important;
+}
+
+@media only screen and (max-width: 650px) and (min-width: 10px) {
+  h2 {
+    padding-right: 0;
+    padding-left: 0;
+    font-size: 22px;
+    margin-top: -40px;
+  }
+  img {
+    padding-right: 0;
+    padding-left: 0;
+  }
+}
+
+@media only screen and (max-width: 1000px) and (min-width: 651px) {
+  h2 {
+    font-size: 20px;
+    padding-right: 5px;
+    line-height: 25px;
+  }
+  img {
+    width: 80%;
+    margin-top: 30px;
+    padding-left: 5px;
+  }
+}
+
+@media only screen and (max-width: 1200px) and (min-width: 1000px) {
+  h2 {
+    font-size: 30px;
+    padding-right: 5px;
+    line-height: 33px;
+  }
+  img {
+    width: 80%;
+    margin-top: 40px;
+    padding-left: 5px;
+  }
+}
+
+@media only screen and (max-width: 1300px) and (min-width: 1200px) {
+  h2 {
+    font-size: 30px;
+    padding-right: 0px;
+    line-height: 33px;
+    padding-top: 40px;
+  }
+}
+
+@media (min-width: 1300) {
+  h2 {
+    font-size: 30px;
+    padding-right: 0px;
+    line-height: 33px;
+    padding-top: 40px;
+  }
+}
+</style>
