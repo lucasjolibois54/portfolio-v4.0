97ac2eda85b8956783bb3ee9b20f7fcd90bb5016
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-24 20:03:26 +0200

    Create YbrntMeaning.vue

---
+++ src/components/YBRNT/YbrntMeaning.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="width-hero">
+    <div class="flex flex-wrap overflow-hidden mt-36 mb-36 ml-10 mr-10">
+      <div class="w-full overflow-hidden md:w-1/2 lg:w-1/2 xl:w-1/2">
+        <!-- Column Content -->
+        <div class="flex flex-col justify-center items-center">
+          <img
+            class="scroll-img w-11/12 h-auto pl-20"
+            :src="require('@/assets/ybrnt-styling-two.gif')"
+          />
+        </div>
+      </div>
+
+      <div class="w-full overflow-hidden md:w-1/2 lg:w-1/2 xl:w-1/2">
+        <!-- Column Content -->
+        <h2 class="text-2xl pr-40 pt-10">
+          I started off by downloading WP locally, where I afterwards opened it
+          up through MAMP, which gave me The possibility to work on the WP theme
+          on a local server. Potentially This gave me more freedom when it came
+          to coding the styling on the theme.
+        </h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+.width-hero {
+  max-width: 1400px;
+  margin: 0 auto !important;
+  float: none !important;
+}
+
+@media only screen and (max-width: 650px) and (min-width: 10px) {
+  h2 {
+    padding-right: 0;
+    padding-left: 0;
+    font-size: 22px;
+  }
+  img {
+    padding-right: 0;
+    padding-left: 0;
+  }
+}
+
+@media only screen and (max-width: 1000px) and (min-width: 651px) {
+  h2 {
+    font-size: 20px;
+    padding-right: 5px;
+    line-height: 25px;
+  }
+  img {
+    width: 80%;
+    margin-top: 30px;
+    padding-left: 5px;
+  }
+}
+
+@media only screen and (max-width: 1200px) and (min-width: 1000px) {
+  h2 {
+    font-size: 30px;
+    padding-right: 5px;
+    line-height: 33px;
+  }
+  img {
+    width: 80%;
+    margin-top: 40px;
+    padding-left: 5px;
+  }
+}
+
+@media only screen and (max-width: 1300px) and (min-width: 1200px) {
+  h2 {
+    font-size: 30px;
+    padding-right: 0px;
+    line-height: 33px;
+    padding-top: 40px;
+  }
+}
+
+@media (min-width: 1300) {
+  h2 {
+    font-size: 30px;
+    padding-right: 0px;
+    line-height: 33px;
+    padding-top: 40px;
+  }
+}
+</style>
