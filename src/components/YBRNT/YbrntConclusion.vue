792876cf3aeca1a037c51a45cd1ac9e0a04248b2
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-24 20:03:39 +0200

    Create YbrntConclusion.vue

---
+++ src/components/YBRNT/YbrntConclusion.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="bg-gray-800">
+    <div class="width-hero">
+      <div class="bg-gray-800 text-white">
+        <h2
+          class="
+            text-4xl
+            flex flex-col
+            justify-center
+            items-center
+            pr-36
+            pl-36
+            pt-16
+            pb-1
+          "
+        >
+          Conclusion
+        </h2>
+
+        <div>
+          <h3
+            class="
+              text-2xl
+              flex flex-col
+              justify-center
+              items-center
+              pr-36
+              pl-36
+              pt-5
+              pb-8
+            "
+          >
+            While working at Global Scanning, I’ve gotten pretty familiar with
+            WordPress, though creating a webshop while also styling it from the
+            local serving using SQL, was something that I hadn’t tried before,
+            so im glad that everything went well. I ended up with a nice small
+            poster webshop which also could get accessed online through WP once
+            uploaded.
+          </h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.width-hero {
+  max-width: 1400px;
+  margin: 0 auto !important;
+  float: none !important;
+}
+
+@media only screen and (max-width: 650px) and (min-width: 10px) {
+  h3 {
+    font-size: 17px;
+    padding-right: 15px;
+    padding-left: 15px;
+  }
+  h2 {
+    font-size: 27px;
+  }
+}
+</style>
