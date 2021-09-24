7cc3bee2944e11daa63fe62d2da3b6b0181b9237
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-24 13:30:49 +0200

    Create Projects.vue

---
+++ src/views/Projects.vue
@@ -0,0 +1,51 @@
+
+
+<template>
+  <div>
+    <NavBar />
+    <ProjectsSection />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ProjectsSection from "@/components/AllProjects/ProjectsSection.vue";
+import NavBar from "@/components/NavBar.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "Home",
+  components: {
+    ProjectsSection,
+    NavBar,
+    Footer,
+  },
+  data() {
+    return {
+      scrollPosition: null,
+    };
+  },
+  methods: {
+    updateScroll() {
+      this.scrollPosition = window.scrollY;
+    },
+  },
+  mounted() {
+    window.addEventListener("scroll", this.updateScroll);
+  },
+};
+</script>
+<style scoped>
+.home {
+  background-color: #000;
+  color: white;
+}
+
+.changecolor {
+  background-color: white;
+  color: black;
+  border: black;
+  transition: 1.5s;
+}
+</style>
