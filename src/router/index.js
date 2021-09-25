c9f46af327d87841f4db68f73ed6668558e9f443
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-25 21:16:57 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -46,6 +46,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutMe.vue')
+  }, {
+    path: '/Shape',
+    name: 'Shape',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Shape.vue')
   },
 ]
 
