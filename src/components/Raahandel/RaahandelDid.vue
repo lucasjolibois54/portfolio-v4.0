1c67b4a6efe6404353d51c877741ad2b3aff636a
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-26 00:14:38 +0200

    Update RaahandelDid.vue

--- src/components/Raahandel/RaahandelDid.vue
+++ src/components/Raahandel/RaahandelDid.vue
@@ -3,7 +3,7 @@
     <div class="flex flex-wrap overflow-hidden mt-36 mb-36 ml-10 mr-10 nam-div">
       <div class="w-full overflow-hidden md:w-1/2 lg:w-1/2 xl:w-1/2">
         <!-- Column Content -->
-        <h2 class="text-2xl pl-40 pt-10">
+        <h2 class="text-2xl h2-pad pt-10">
           I started off with the usual research by folllowing the process method
           design thinking. Afterwards I started redesigning their website, and
           tried to understand their customer journey as well as which elements
@@ -39,11 +39,28 @@
     padding-right: 0;
     padding-left: 0;
     font-size: 22px;
-    margin-top: -40px;
+    padding-top: 0;
+    text-align: center;
   }
   img {
     padding-right: 0;
     padding-left: 0;
+  }
+}
+
+@media only screen and (max-width: 767) and (min-width: 651px) {
+  h2 {
+    font-size: 20px;
+    padding-right: 5px;
+    padding-left: 0px;
+    line-height: 25px;
+    text-align: center;
+  }
+
+  img {
+    width: 80%;
+    margin-top: 30px;
+    padding-left: 5px;
   }
 }
 
@@ -52,24 +69,28 @@
     font-size: 20px;
     padding-right: 5px;
     line-height: 25px;
+    padding-left: 2rem;
   }
   img {
     width: 80%;
     margin-top: 30px;
     padding-left: 5px;
+    padding-top: 40px;
   }
 }
 
 @media only screen and (max-width: 1200px) and (min-width: 1000px) {
   h2 {
-    font-size: 30px;
+    font-size: 26px;
     padding-right: 5px;
     line-height: 33px;
+    padding-left: 2px;
   }
   img {
     width: 80%;
     margin-top: 40px;
     padding-left: 5px;
+    padding-top: 60px;
   }
 }
 
@@ -79,15 +100,20 @@
     padding-right: 0px;
     line-height: 33px;
     padding-top: 40px;
+    padding-left: 3rem;
+  }
+  img {
+    padding-top: 30px;
   }
 }
 
-@media (min-width: 1300) {
+@media only screen and (max-width: 1500px) and (min-width: 1300px) {
   h2 {
     font-size: 30px;
     padding-right: 0px;
     line-height: 33px;
-    padding-top: 40px;
+    padding-top: 0px;
+    padding-left: 8rem;
   }
 }
 </style>
