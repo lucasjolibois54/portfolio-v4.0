48a5761fa14ce6ea986077a9d8058317fc55195d
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-26 00:14:40 +0200

    Update ShapeDone.vue

--- src/components/Shape-up/ShapeDone.vue
+++ src/components/Shape-up/ShapeDone.vue
@@ -3,7 +3,7 @@
     <div class="flex flex-wrap overflow-hidden mt-36 mb-36 ml-10 mr-10 nam-div">
       <div class="w-full overflow-hidden md:w-1/2 lg:w-1/2 xl:w-1/2">
         <!-- Column Content -->
-        <h2 class="text-2xl pl-40 pt-10">
+        <h2 class="h2-pad text-2xl pt-10">
           After my research I started designing The logo. As the brand is called
           shape It up, so I wanted to focus on just that. The logo has an old
           school lifter, holding a barbell while there’s an arrow, pointing up
@@ -18,7 +18,7 @@
 
         <div class="flex flex-col justify-center items-center">
           <img
-            class="scroll-img w-11/12 h-auto pr-20"
+            class="scroll-img w-11/12 h-auto"
             :src="require('@/assets/shape-last-two.gif')"
           />
         </div>
@@ -41,10 +41,27 @@
     padding-left: 0;
     font-size: 22px;
     padding-top: 0;
+    text-align: center;
   }
   img {
     padding-right: 0;
     padding-left: 0;
+  }
+}
+
+@media only screen and (max-width: 767) and (min-width: 651px) {
+  h2 {
+    font-size: 20px;
+    padding-right: 5px;
+    padding-left: 0px;
+    line-height: 25px;
+    text-align: center;
+  }
+
+  img {
+    width: 80%;
+    margin-top: 30px;
+    padding-left: 5px;
   }
 }
 
@@ -53,24 +70,34 @@
     font-size: 20px;
     padding-right: 5px;
     line-height: 25px;
+    padding-left: 2rem;
   }
   img {
     width: 80%;
     margin-top: 30px;
     padding-left: 5px;
+    padding-top: 40px;
+  }
+}
+@media only screen and (max-width: 1000px) and (min-width: 770px) {
+  img {
+    padding-right: 2rem;
   }
 }
 
 @media only screen and (max-width: 1200px) and (min-width: 1000px) {
   h2 {
-    font-size: 30px;
+    font-size: 26px;
     padding-right: 5px;
     line-height: 33px;
+    padding-left: 2px;
   }
   img {
     width: 80%;
     margin-top: 40px;
     padding-left: 5px;
+    padding-top: 60px;
+    padding-right: 5rem;
   }
 }
 
@@ -80,15 +107,24 @@
     padding-right: 0px;
     line-height: 33px;
     padding-top: 40px;
+    padding-left: 3rem;
+  }
+  img {
+    padding-top: 30px;
+    padding-right: 5rem;
   }
 }
 
-@media (min-width: 1300) {
+@media only screen and (max-width: 1500px) and (min-width: 1300px) {
   h2 {
     font-size: 30px;
     padding-right: 0px;
     line-height: 33px;
-    padding-top: 40px;
+    padding-top: 0px;
+    padding-left: 8rem;
+  }
+  img {
+    padding-right: 5rem;
   }
 }
 </style>
