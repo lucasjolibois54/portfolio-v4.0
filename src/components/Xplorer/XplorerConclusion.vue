912f83b37ff9de3c3c2d074f7598841cc40691fa
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-27 21:45:26 +0200

    Create XplorerConclusion.vue

---
+++ src/components/Xplorer/XplorerConclusion.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="bg-gray-800">
+    <div class="width-hero">
+      <div class="bg-gray-800 text-white">
+        <h2
+          class="
+            text-4xl
+            flex flex-col
+            justify-center
+            items-center
+            pr-36
+            pl-36
+            pt-16
+            pb-1
+          "
+        >
+          Conclusion
+        </h2>
+
+        <div>
+          <h3
+            class="
+              text-2xl
+              flex flex-col
+              justify-center
+              items-center
+              pr-36
+              pl-36
+              pt-5
+              pb-8
+            "
+          >
+            Creating this logo was pretty fun. I don’t design & create logos
+            that often, so I’m glad I got the possibility to design this.
+            Additionally I’m also pretty glad about the results. My results
+            ended up looking exacly like what I was going for. The design looks
+            clean, and it’s easy to understand what the brand does and it's
+            related to.
+          </h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.width-hero {
+  max-width: 1400px;
+  margin: 0 auto !important;
+  float: none !important;
+}
+
+@media only screen and (max-width: 650px) and (min-width: 10px) {
+  h3 {
+    font-size: 17px;
+    padding-right: 15px;
+    padding-left: 15px;
+  }
+  h2 {
+    font-size: 27px;
+  }
+}
+</style>
