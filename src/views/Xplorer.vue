66a2ae80dd4446052d0ca4352fa2895b0d4936ad
lucasjollibois54 <74597842+lucasjollibois54@users.noreply.github.com>
2021-09-27 21:45:20 +0200

    Update Xplorer.vue

--- src/views/Xplorer.vue
+++ src/views/Xplorer.vue
@@ -4,7 +4,10 @@
   <div>
     <NavBar :class="{ changecolor: scrollPosition > 70 }" class="home" />
     <XplorerHero :class="{ changecolor: scrollPosition > 70 }" class="home" />
-
+    <XplorerMeaning />
+    <XplorerHow />
+    <XplorerDid />
+    <XplorerConclusion />
     <Footer />
   </div>
 </template>
@@ -13,13 +16,21 @@
 // @ is an alias to /src
 import XplorerHero from "@/components/Xplorer/XplorerHero.vue";
 import NavBar from "@/components/NavBar.vue";
+import XplorerMeaning from "@/components/Xplorer/XplorerMeaning.vue";
 import Footer from "@/components/Footer.vue";
+import XplorerHow from "@/components/Xplorer/XplorerHow.vue";
+import XplorerDid from "@/components/Xplorer/XplorerDid.vue";
+import XplorerConclusion from "@/components/Xplorer/XplorerConclusion.vue";
 
 export default {
   name: "Home",
   components: {
     NavBar,
+    XplorerConclusion,
     XplorerHero,
+    XplorerMeaning,
+    XplorerDid,
+    XplorerHow,
     Footer,
   },
   data() {
